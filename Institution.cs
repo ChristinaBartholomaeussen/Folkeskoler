cfef5bf966b143214ad056a249671f4a53891c8c
christina bartholomæussen <c.m.bartholo@gmail.com>
2021-10-11 13:00:30 +0200

    Add project files.

---
+++ Institution.cs
@@ -0,0 +1,30 @@
+﻿
+namespace readfromexcel_01
+{
+    class Institution
+    {
+        
+        public string INST_NAVN { get; set; }
+
+        public string INST_ADR { get; set; }
+        public string POSTNR { get; set; }
+        public string POSTDISTRIKT { get; set; }
+        public string TLF_NR { get; set; }
+
+        public override string ToString()
+        {
+            
+            string insName = string.Format("{0,-65}", INST_NAVN);
+            string address = string.Format("{0,-30}", INST_ADR);
+            string zipcode = string.Format("{0,-4}", POSTNR);
+            string postd = string.Format("{0,-15}", POSTDISTRIKT);
+            string phonenumber = string.Format("{0,-8}", TLF_NR);
+
+            return insName + " " + address + " " + zipcode +
+                " " + postd + " " + phonenumber;
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Ganss.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace readfromexcel_01
+{
+    class Program
+    {
+        static string file = "FolkeskoleDK13-09-2021.xlsx";
+        static void Main(string[] args)
+        {
+          
+            do
+            {
+
+                IEnumerable<Institution> institutions = new ExcelMapper(file).Fetch<Institution>();
+
+                Console.Write("Start med at skrive navnet på skolen: ");
+                string insName = Console.ReadLine();
+              
+                IEnumerable<Institution> selectedSchools = institutions.Where(i => i.INST_NAVN.StartsWith(insName, StringComparison.InvariantCultureIgnoreCase));
+
+                if (!selectedSchools.Any())
+                {
+                    Console.WriteLine("Ingen skoler fundet...");
+                }
+                else
+                {
+                    Console.WriteLine("Følgende skoler er fundet");
+                    foreach (Institution ins in selectedSchools)
+                    {
+                        
+                        Console.WriteLine(ins);
+                    }
+                }
+
+            } while (Console.ReadKey().KeyChar != 'x');
+
+        }
+    }
+}
